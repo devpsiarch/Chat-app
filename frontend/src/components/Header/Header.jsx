773587c8eb1f185142f4806753e262f415b6669f
devpsiarch <hmayoubaty@gmail.com>
2024-09-10 23:31:40 +0100

    UI improve

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.scss";
+
+
+//in react we work with components sepretlly ...
+const Header = () => (
+	<div className="Header">
+		<h2>Real time chat application</h2>
+	</div>
+);
+
+export default Header;
