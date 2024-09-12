23b0bbf757ce4339195e638e8f745db564d64113
devpsiarch <hmayoubaty@gmail.com>
2024-09-12 17:50:36 +0100

    handelling many users

--- backend/main.go
+++ backend/main.go
@@ -2,59 +2,25 @@ package main
 
 import (
     "fmt"
-    "log"
     "net/http"
-    "github.com/gorilla/websocket"
+    "github.com/devpsiarch/Chat-app.git/websocket"
 ) 
 
-func check(err error){
+//def websocket endpoint
+func Serve_ws(w http.ResponseWriter, r *http.Request){
+    fmt.Println(r.Host)
+    //upgrading to ws
+    ws,err := upgrader.Upgrade(w,r,nil)
     if err != nil {
-        log.Println(err)
-        return
+        fmt.Fprintf(w, "%+V\n", err)
     }
-}
-
-//this is our upgrader it will upgarade the connection to websocket (ws)
-//needs reader and a readbuffer
-var upgrader =  websocket.Upgrader{
-    ReadBufferSize:  1024,
-    WriteBufferSize: 1024,
-    //impliment checking to allow requests from react frontend
-    //for this allows all connections 
-    CheckOrigin: func(r *http.Request) bool {return true},
-}
-
-//this bad boy will listen for messages sent to out websocket
-//needs a websocket connections
-func reader(conn *websocket.Conn){
-    for{
-        //receives the message 
-        message_type,p,err := conn.ReadMessage()
-        check(err)
-        //lets the messgae
-        fmt.Println(string(p))
-        err = conn.WriteMessage(message_type,p)
-        check(err)
-    }
+    go websocket.Writer(ws)
+    websocket.Reader(ws)
 }
 
 //the function defined below is a handler 
 func setuproutes() {
-    http.HandleFunc("/",
-    func(w http.ResponseWriter, r *http.Request){
-        fmt.Fprintf(w, "hello from simple server")
-    })
-    http.HandleFunc("/ws",ws_endpoint)
-}
-
-//def websocket endpoint
-func ws_endpoint(w http.ResponseWriter, r *http.Request){
-    fmt.Println(r.Host)
-    //upgrading to ws
-    ws,err := upgrader.Upgrade(w,r,nil)
-    check(err)
-    //read from ws
-    reader(ws)
+    http.HandleFunc("/ws",Serve_ws)
 }
 
 
